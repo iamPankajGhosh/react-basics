4ad159731544a4efa59a03b45777c07d322a2bb0
Pankaj Ghosh <pankajghosh.github@gmail.com>
2023-12-24 22:58:18 +0530

    tailwind project done

---
+++ 05_tailwind_project/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind test</h1>
+      <Card username="pankaj ghosh" btnText="Read me" />
+      <Card username="javascript" />
+    </>
+  );
+}
+
+export default App;
