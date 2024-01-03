8a6aa0b3cb17b3947a2125f09916f832dba5b168
Pankaj Ghosh <pankajghosh.github@gmail.com>
2024-01-03 13:01:46 +0530

    react hook form done

--- 14_megablog_project/src/components/index.js
+++ 14_megablog_project/src/components/index.js
@@ -5,5 +5,21 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
+import Select from "./Select";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  Select,
+  PostCard,
+  Login,
+  Signup,
+};
